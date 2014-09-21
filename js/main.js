45a82459860cb8057d4e8e87f90f3e8b853a7883
Taras Volchenko <votaras@yandex.ru>
2014-09-21 22:26:01 +0700

    Some shit happens

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ function preload() {
 
     game.load.image('background','assets/background.png');
     game.load.image('player','assets/hero.png');
-
+    game.load.image('enemy','assets/enemy.png');
 }
 
 var player;
 var cursors;
+var spacebarKey;
+var enemies;
 
 function create() {
 
@@ -22,13 +24,18 @@ function create() {
 
     game.physics.p2.enable(player);
 
+    enemies = game.add.group();
+    for (var i = 0; i < 100; i++) {
+        var enemy = enemies.create(game.rnd.integerInRange(0, 4992), game.rnd.integerInRange(0, 3072), 'enemy');
+        game.physics.p2.enable(enemy,false);
+    }
     cursors = game.input.keyboard.createCursorKeys();
-
+    spacebarKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     game.camera.follow(player);
-
 }
+var forceAway = 0;
 
-function update() {
+function update(){
 
     player.body.setZeroVelocity();
 
@@ -49,7 +56,23 @@ function update() {
     {
         player.body.moveRight(300);
     }
+    
+    if((spacebarKey.isDown)&&(forceAway ===0)){
+          forceAway = 600;
+    }
+    forceAway = Math.max(0,forceAway-1);
+    enemies.forEachAlive(
+        function(enemy){
+            accelerateToObject(enemy, player, Math.min((-forceAway + 500),0));
+    });
+}
 
+function accelerateToObject(obj1, obj2, speed) {
+    if (typeof speed === 'undefined') { speed = 60; }
+    var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
+    obj1.body.rotation = angle + game.math.degToRad(90);
+    obj1.body.force.x = Math.cos(angle) * speed;    
+    obj1.body.force.y = Math.sin(angle) * speed;
 }
 
 function render() {
